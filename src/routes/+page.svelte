51f9b5f7da5714ecfd8977d461a78e23f2553cda
Remy Duivesteijn <remyd03@icloud.com>
2024-12-16 19:41:36 +0100

    v6.0 | image en about component static 📸

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,42 +4,7 @@
     export let data;
 </script>
 
-<!-- <Navigation /> -->
-
-<!-- <div class="container"> -->
     <Heading />
     <Grid {data} />
     <About />
     <Credits />
-<!-- </div> -->
-
-<!-- <Footer />
-
-<style>
-    .container {
-        width: 100%;
-        background: var(--container-gradient);
-        border-radius: 1.563rem;
-        padding: 3.125rem 2.188rem;
-    }
-
-    @media screen and (min-width: 768px) {
-        .container {
-            border-radius: 1.875rem;
-            padding: 1rem 3.125rem 1rem;
-        }
-    }
-
-    @media screen and (min-width: 1024px) {
-        .container {
-            border-radius: 2.5rem;
-            padding: 1rem 6.25rem 2rem;
-        }
-    }
-
-    @media screen and (min-width: 1440px) {
-        .container {
-            padding: 1rem 10rem 2rem;
-        }
-    }
-</style> -->
--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -1,1 +1,81 @@
-Detail page!
+<img src="/img/kv.png" alt="" />
+<article>
+    <h2>Kilian Valkhof</h2>
+    <h3>Developer</h3>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit
+        posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
+        facilisis, facilisis felis. Duis facilisis arcu pharetra Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit.<br /><br />Nunc suscipit
+        posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
+        facilisis, facilisis felis. Duis facilisis arcu.
+    </p>
+</article>
+
+<style>
+    img {
+        width: 100%;
+        margin-top: 2.5rem;
+        border-radius: 1rem;
+    }
+
+    h2 {
+        margin-top: 1rem;
+        color: var(--blue);
+    }
+
+    h3 {
+        margin-top: 5px;
+        font-size: 1rem;
+        font-weight: var(--font-weight-semi);
+        color: var(--blue);
+    }
+
+    p {
+        margin-top: 10px;
+        font-size: 1rem;
+        font-weight: var(--font-weight-semi);
+        color: rgb(5 5 66 / 65%);
+    }
+
+    @media screen and (min-width: 768px) {
+        img {
+            border-radius: 1.25rem;
+        }
+
+        h2 {
+            margin-top: 1.563rem;
+            font-size: 1.875rem;
+        }
+
+        h3 {
+            margin-top: 10px;
+            font-size: 1.125rem;
+        }
+
+        p {
+            margin-top: 1rem;
+            font-size: 1.375rem;
+        }
+    }
+
+    @media screen and (min-width: 1440px) {
+        img {
+            border-radius: 1.5rem;
+        }
+        h2 {
+            margin-top: 3.125rem;
+            font-size: 2.5rem;
+        }
+
+        h3 {
+            margin-top: 5px;
+            font-size: 1.375rem;
+        }
+
+        p {
+            margin-top: 1.875rem;
+            font-size: 1.5rem;
+        }
+    }
+</style>
