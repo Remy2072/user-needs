b4af7b43ef6b91d0b5d03ad94af00018ab247a8d
Jaymar Westerlow <jaywesterlow71@gmail.com>
2024-12-17 23:43:08 +0100

    v6.2 | Subjects & Links ✨

--- src/lib/structuur/Svg.svelte
+++ src/lib/structuur/Svg.svelte
@@ -297,6 +297,14 @@
     </defs>
   </svg>
 
+  {:else if name == 'arrow-right'}
+
+  <svg width="24" height="24" viewBox="2 2 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 12H19" stroke="#050542" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 5L19 12L12 19" stroke="#050542" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+
   <!-- {:else if name == ''} -->
 {/if}
 
--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { Anchor } from "$lib";
+    import { Svg } from "$lib";
 </script>
 
 <header>
@@ -12,18 +12,42 @@
 </header>
 
 <img src="/img/kv.png" alt="" />
-<article>
-    <h2>Kilian Valkhof</h2>
-    <h3>Developer</h3>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit
-        posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
-        facilisis, facilisis felis. Duis facilisis arcu pharetra Lorem ipsum
-        dolor sit amet, consectetur adipiscing elit.<br /><br />Nunc suscipit
-        posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
-        facilisis, facilisis felis. Duis facilisis arcu.
-    </p>
-</article>
+<section>
+    <article>
+        <h2>Kilian Valkhof</h2>
+        <h3>Developer</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc suscipit
+            posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
+            facilisis, facilisis felis. Duis facilisis arcu pharetra Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit.<br /><br />Nunc suscipit
+            posuere tellus ac sodales. Fusce hendrerit elit dictum, mattis ipsum
+            facilisis, facilisis felis. Duis facilisis arcu.
+        </p>
+    </article>
+    
+    <aside>
+        <div>
+            <h3>Onderwerp(en)</h3>
+            <ul>
+                <li>SVELTE</li>
+                <li>AI</li>
+                <li>ONTWERPEN</li>
+                <li>HOSTING</li>
+                <li>SEO</li>
+            </ul>
+        </div>
+    
+        <nav>
+            <h3>Nuttige link(s)</h3>
+            <ul>
+                <li><a href="https://www.hva.nl/"><span>HvA</span><Svg name="arrow-right" /></a></li>
+                <li><a href="https://fdnd.nl/"><span>FDND</span><Svg name="arrow-right" /></a></li>
+            </ul>
+        </nav>
+    </aside>
+</section>
+
 
 <style>
     header {
@@ -59,7 +83,7 @@
         }
     }
 
-    header a {
+    header > a {
         position: relative;
         z-index: 1;
         border: 1px solid var(--blue);
@@ -81,12 +105,12 @@
         }
     }
 
-    header a:hover {
+    header > a:hover {
         top: 1px;
         right: 1px;
     }
 
-    header a::after {
+    header > a::after {
         content: "";
         z-index: -1;
         position: absolute;
@@ -102,7 +126,7 @@
             right 0.2s;
     }
 
-    header a:hover::after {
+    header > a:hover::after {
         top: 1px;
         right: 1px;
     }
@@ -113,9 +137,17 @@
         border-radius: 1rem;
     }
 
+    section {
+        margin-top: 1rem;
+        display: flex;
+        flex-direction: column;
+
+        @media (min-width: 1024px) {
+            flex-direction: row;
+        }
+    }
 
     h2 {
-        margin-top: 1rem;
         color: var(--blue);
     }
 
@@ -131,15 +163,120 @@
         font-size: 1rem;
         font-weight: var(--font-weight-semi);
         color: rgb(5 5 66 / 65%);
+        margin-bottom: 1rem;
     }
 
+    aside {
+        display: flex;
+        justify-content: space-around;
+        position: relative;
+        min-width: 25%;
+
+        @media (min-width: 1024px) {
+            flex-direction: column;
+            
+            ::before {
+                content: "";
+                position: absolute;
+                height: 100%;
+                width: 2px;
+                background-color: #E9E0E9;
+                left: -1rem;
+                top: 0;
+            }
+        }
+    }
+
+    aside h3 {
+        opacity: 45%;
+        margin-bottom: 1.5rem;
+    }
+
+    aside div {
+
+    }
+
+    aside div ul {
+        list-style-type: none;
+        display: flex;
+        flex-wrap: wrap;
+        gap: .4rem;
+        max-width: 80%;
+
+        @media (min-width: 1024px) {
+            max-width: 100%;
+        }
+    }
+
+    aside div ul li {
+        padding: .2rem 1rem;
+        background-color: red;
+        border-radius: 5rem;
+        font-weight: 600;
+        color: var(--blue);
+    }
+
+    aside div ul li:nth-child(1) {
+        background-color: var(--yellow);
+    }
+    aside div ul li:nth-child(2) {
+        background-color: var(--green);
+    }
+    aside div ul li:nth-child(3) {
+        background-color: var(--purple);
+        color: var(--white);
+    }
+    aside div ul li:nth-child(4) {
+        background-color: #FF86F9;
+    }
+    aside div ul li:nth-child(5) {
+        background-color: #86BCFF;
+        color: var(--white);
+    }
+
+    aside nav {
+        min-width: 30%;
+    }
+
+    aside nav ul {
+        list-style-type: none;
+        color: var(--blue);
+        font-weight: 500;
+    }
+
+    aside nav ul li {
+        margin-bottom: 1rem;
+        transition: .3s ease-in-out;
+        width: fit-content;
+    }
+    
+    aside nav ul li a {
+        font-size: 1.5rem;
+        font-weight: 600;
+        color: var(--blue);
+        display: flex;
+        align-items: center;
+    }
+    aside nav ul li a span{
+        margin-right: .8rem;
+    }
+
+    aside nav ul li:hover {
+            margin-left: 5px;
+            opacity: 85%;
+            transition: .3s ease-in-out;
+        }
+
     @media screen and (min-width: 768px) {
+        section {
+            margin-top: 1.563rem;
+        }
+        
         img {
             border-radius: 1.25rem;
         }
 
         h2 {
-            margin-top: 1.563rem;
             font-size: 1.875rem;
         }
 
@@ -154,12 +291,21 @@
         }
     }
 
+    @media screen and (min-width: 1024px) {
+        p {
+            max-width: 95%;
+        }
+    }
+
     @media screen and (min-width: 1440px) {
+        section {
+            margin-top: 3.125rem;
+        }
+        
         img {
             border-radius: 1.5rem;
         }
         h2 {
-            margin-top: 3.125rem;
             font-size: 2.5rem;
         }
 
